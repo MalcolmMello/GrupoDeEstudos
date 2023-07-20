23f55d6dcbffcac0577d779b77af1a52026562fc
MalcolmMello <malcolmmellolima@gmail.com>
2023-07-20 15:01:45 -0300

    store cookie using redis

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,38 @@
-import { Module } from '@nestjs/common';
+import { Inject, MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AuthModule } from '@infra/auth/auth.module';
 import { DatabaseModule } from '@infra/database/database.module';
 import { HttpModule } from '@infra/http/http.module';
 import { ConfigModule } from '@nestjs/config';
+import { RedisModule } from './redis/redis.module';
+import { REDIS } from './redis/redis.constants';
+import { RedisClientType } from 'redis';
+import RedisStore from 'connect-redis';
+import * as session from 'express-session';
+import * as passport from 'passport';
 
 @Module({
-  imports: [AuthModule, HttpModule, DatabaseModule, ConfigModule.forRoot()]
+  imports: [AuthModule, RedisModule, HttpModule, DatabaseModule, ConfigModule.forRoot()]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  constructor(@Inject(REDIS) private readonly redis: RedisClientType) {}
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(
+        session({
+          store: new RedisStore({ client: this.redis}),
+          saveUninitialized: false,
+          secret: process.env.SESSION_SECRET,
+          resave: false,
+          cookie: {
+            sameSite: true,
+            httpOnly: false,
+            maxAge: 6000,
+          },
+        }),
+        passport.initialize(),
+        passport.session(),
+      )
+      .forRoutes('*');
+  }
+}
+
---
+++ backend/src/redis/redis.module.ts
@@ -0,0 +1,18 @@
+import { Module } from "@nestjs/common";
+import * as Redis from 'redis';
+import { REDIS } from './redis.constants';
+
+@Module({
+  providers: [
+    {
+      provide: REDIS,
+      useFactory: async () => {
+        const client = Redis.createClient({ url: 'redis://localhost:6379', legacyMode: false })
+        await client.connect();
+        return client;
+      }
+    }
+  ],
+  exports: [REDIS]
+})
+export class RedisModule {}
